a6011ba896caca3206180b1704ef3e4f9755f5a8
Gamanov Konstantin <gamanov@skbkontur.ru>
2022-06-20 13:43:57 +0500

    Выпилил старые методы, в том числе, которые были в сторону введения сразу 2х игр

--- Main.py
+++ Main.py
@@ -4,9 +4,7 @@ import telebot
 
 
 bot = telebot.TeleBot("509161010:AAGcICZGPTiQTLJJJlScKszY5uUyo-bByuQ")
-
 db = sqlite3.connect('server.db', check_same_thread=False)
-
 sql = db.cursor()
 
 # ВОТ ЭТО НУЖНО УДАЛИТЬ ОБЯЗАТЕЛЬНО
@@ -14,65 +12,29 @@ sql = db.cursor()
 SqlBase.DataBase.DropDataBase(sql,db)
 print('УДАЛИ ЭТО ОБЯЗАТЕЛЬНО')
 
-# И нужны методы обвязки, которые будут включать в себя бота и команды SQL
-#Бота надо как-то растащить на методы
-
-
-
-
-#первое действие человека, который зашел в бота
 @bot.message_handler(commands=['start'])
 def start_message(message):
-    user_markup = telebot.types.ReplyKeyboardMarkup(True)
+    markup = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
+    rebus = telebot.types.KeyboardButton("Играть")
+    liderBoard = telebot.types.KeyboardButton("Список лидеров")
+
     SqlBase.DataBase.CreateDataBase(sql, db)
     SqlBase.DataBase.InsertNewUser(message.from_user.username, sql, db)
 
-    SqlBase.DataBase.SelectFromDataBase(sql)
-    user_markup.row('/button')
-    bot.send_message(message.from_user.id, "Привет, ткни в /button", reply_markup=user_markup)
-
-
-
-
-
-
-#Рисует кнопки
-@bot.message_handler(commands=['button'])
-def button_message(message):
-    markup = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
-    rebus = telebot.types.KeyboardButton("Ребус")
-    Button2 = telebot.types.KeyboardButton("Кнопка2")
     markup.add(rebus)
-    markup.add(Button2)
+    markup.add(liderBoard)
+
     bot.send_message(message.chat.id, 'Выбери тему', reply_markup=markup)
 
 
 
-
-#Реагирует на кнопки
-#По клику на "Кнопка1" вызывает рандомную картинку из базы
 @bot.message_handler(content_types='text')
 def message_reply(message):
-    if message.text == "Ребус":
+    if message.text == "Играть":
         SentNextRebus(message, sql)
-        #nextRebus = SqlBase.DataBase.NextRebus(message.from_user.username, sql)
-       # bot.send_message(message.chat.id, 'Фоточка грузится')
-        #bot.send_photo(message.chat.id, nextRebus)
-       # msg = bot.send_message(message.chat.id, 'Введи правильный ответ по русски')
-
-        #bot.register_next_step_handler(msg, after_text_2, nextRebus)
-    #elif message.text == "Исключи лишнее":
-        #Заглушка
-    #else
-        #bot.send_message(message.chat.id, 'Таких вариантов у меня нет, попробуй еще')
 
 
-
-
-
-
-
-def after_text_2(message, nextRebus):
+def AnswerValidator(message, nextRebus):
 
     print('Второй этап запустился')
     print(SqlBase.DataBase.SelectCorrectAnswer(db, nextRebus))
@@ -89,11 +51,10 @@ def after_text_2(message, nextRebus):
 
 def SentNextRebus(message, sql):
     nextRebus = SqlBase.DataBase.NextRebus(message.from_user.username, sql)
-    #bot.send_message(message.chat.id, 'Фоточка грузится')
     bot.send_photo(message.chat.id, nextRebus)
     msg = bot.send_message(message.chat.id, 'Введи правильный ответ по русски')
 
-    bot.register_next_step_handler(msg, after_text_2, nextRebus)
+    bot.register_next_step_handler(msg, AnswerValidator, nextRebus)
 
 
 bot.polling(none_stop=True, interval=0)
